32606ccb2b6ab65cb10e2d2f5bd668b33976e1b0
yang107-Xiao <azalebutler2369@gmail.com>
2025-11-22 13:00:31 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // 引入 CSS 样式，确保路径正确指向根目录下的 index.css
-import '../index.css' 
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
